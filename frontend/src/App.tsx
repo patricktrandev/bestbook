934ea64cf802873a99dedf1d0ce32201bb927f35
patricktrandev <patricktrandev@gmail.com>
2024-04-19 06:48:06 +0800

    add carousel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./layouts/NavbarAndFooter/Navbar";
 import ExploreTopBook from "./layouts/HomePage/ExploreTopBook";
+import Carousel from "./layouts/HomePage/Carousel";
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreTopBook />
+      <Carousel />
     </div>
   );
 }
