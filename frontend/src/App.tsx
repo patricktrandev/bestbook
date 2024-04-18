8b254d146b51d1fcab630654b67e4f097938b437
patricktrandev <patricktrandev@gmail.com>
2024-04-18 14:23:31 +0800

    create navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 
 import "./App.css";
+import Navbar from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to full stack app</h1>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
---
+++ frontend/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
+      <div className="container-fluid">
+        <span className="navbar-brand">Luv2 Read</span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Search Books
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item m-1">
+              <a type="button" className="btn btn-outline-light" href="#">
+                Sign In
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
